66de053559cb8a50c0f73f81af14a98721ae3d7c
Ricardo <bigardone@gmail.com>
2015-08-12 17:14:58 +0200

    Person spec and factory

---
+++ spec/factories/people.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+  factory :person do
+    first_name "MyString"
+last_name "MyString"
+email "MyString"
+birth_date "2015-08-07"
+location "MyString"
+headline "MyText"
+gender 1
+phone_number "MyString"
+picture "MyString"
+  end
+
+end
